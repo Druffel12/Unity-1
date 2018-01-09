f708284b1966b47c92eef3ec17fd5bf9c20685f3
Cole Druffel <s179052@students.aie.edu.au>
2018-01-08 16:17:35 -0800

    Initial commit

---
+++ Assets/BallControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    public int score;
+
+    public float health;
+    public float jump;
+
+    public float sprintMultiplier = 1.5f;
+    public float stamina = 100.0f;
+    public float sprintMinimum = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        health = 100;
+        score = 0;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float sprintVal = Input.GetAxis("Sprint");
+
+        float finalSpeed = speed;
+        bool isSprint = sprintVal != 0.0f && stamina > sprintMinimum;
+        if (isSprint)
+        {
+            finalSpeed *= sprintMultiplier;
+            stamina -= 10.0f * Time.deltaTime;
+        }
+        else
+        {
+            stamina += 8.0f * Time.deltaTime;
+        }
+        stamina = Mathf.Clamp(stamina, 0, 100);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+        rb.AddForce(move * speed * Time.deltaTime);
+        rb.AddForce(move * finalSpeed * Time.deltaTime);
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           rb.AddForce(0,jump,0,ForceMode.Impulse);
+        }
+	}   
+}
