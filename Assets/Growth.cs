34f12179b5e6c06473310ebb0f040fcb40edc983
Cole Druffel <s179052@students.aie.edu.au>
2018-01-10 12:03:07 -0800

    WORKED ON GROWTH FUCNTION

--- Assets/Growth.cs
+++ Assets/Growth.cs
@@ -2,36 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Growth : MonoBehaviour {
+public class Growth : MonoBehaviour
+{
 
     public float scaleFactor = 2.0f;
     public float timeToGrow = 3.0f;
 
-
-    IEnumerator Grow()
+    void OnTriggerEnter(Collider other)
     {
-        Vector3 initialScale = transform.localScale;
-        Vector3 finalScale = Vector3.one * scaleFactor;
-        float elapsedTime = 0.0f;
 
-        while(elapsedTime < timeToGrow)
+        if (other.tag == "Player")
         {
-            elapsedTime += Time.deltaTime;
-            float lerpTime = elapsedTime / timeToGrow;
-           transform.localScale = Vector3.Lerp(initialScale,
-                finalScale, lerpTime);
-            yield return null;
+            var playerController = other.GetComponent<BallControl>();
+            Vector3 initialScale = playerController.transform.localScale;
+            Vector3 finalScale = Vector3.one * scaleFactor;
+            float elapsedTime = 0.0f;
+
+            while (elapsedTime < timeToGrow)
+            {
+                elapsedTime += Time.deltaTime;
+                float lerpTime = elapsedTime / timeToGrow;
+                playerController.transform.localScale = Vector3.Lerp(initialScale,
+                      finalScale, lerpTime);
+                yield return null;
+            }
         }
     }
-	// Use this for initialization
-	void Start ()
+
+
+
+
+
+
+    // Use this for initialization
+    void Start()
     {
-        StartCoroutine(Grow());
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+
+    }
 }
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+    public int scoreAdded;
+    public float healthDecreased;
+    public float rotationSpeed;
+    public float grow;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(rotationSpeed, 0, rotationSpeed / 3);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            var playerController = other.GetComponent<BallControl>();
+            if(playerController != null)
+            {
+                playerController.score += scoreAdded;
+                playerController.health += healthDecreased;
+                playerController.grow += grow;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
