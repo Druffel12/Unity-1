75c3a7ae78259d44bade17c2e326cf08a517666f
Cole Druffel <s179052@students.aie.edu.au>
2018-01-16 17:52:03 -0800

    worked on UI layout for player

---
+++ Assets/Scripts/Pick Ups/Growth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Growth : MonoBehaviour
+{
+
+    public float scaleFactor = 2.0f;
+    public float timeToGrow = 3.0f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        var playerController = other.GetComponent<BallControl>();
+
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Grow(other.gameObject));
+        }
+     }
+    
+        
+    IEnumerator Grow(GameObject Player)
+    {
+        Vector3 initialScale = Player.transform.localScale;
+        Vector3 finalScale = Vector3.one * scaleFactor;
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < timeToGrow)
+        {
+            elapsedTime += Time.deltaTime;
+            float lerpTime = elapsedTime / timeToGrow;
+            transform.localScale = Vector3.Lerp(initialScale,
+                 finalScale, lerpTime);
+            yield return null;
+        }
+    }
+
+
+    
+     // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    public Text timerLabel;
+
+    public float duration;
+
+    void Update()
+    {
+
+        duration -= Time.deltaTime;
+
+        int temp = (int)duration;
+
+        int hours = temp / 60 / 60;
+        int minutes = (int) (temp / 60);
+        int seconds = temp % 60;
+
+        timerLabel.text = hours.ToString("D2") + ":" + minutes.ToString("D2") + ":" +
+            seconds.ToString("D2");
+    }
+
+    public void ToggleTimer()
+    {
+        enabled = !enabled;
+    }
+}
